eba8913ebc760bbbbc128189e5b48391f7ed0bc9
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-09-24 15:26:46 +0800

    Multiplies nonequal fractions

--- fractions/src/test/java/MultiplyFractionsTest.java
+++ fractions/src/test/java/MultiplyFractionsTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class MultiplyFractionsTest {
@@ -25,5 +26,11 @@ public class MultiplyFractionsTest {
         Assertions.assertEquals(new Fraction(1, 4), new Fraction(1, 2).times(new Fraction(1, 2)));
         Assertions.assertEquals(new Fraction(1, 4), new Fraction(-1, 2).times(new Fraction(-1, 2)));
         Assertions.assertEquals(new Fraction(9, 25), new Fraction(3, 5).times(new Fraction(3, 5)));
+    }
+
+    @Test
+    void multiplyingNonEqualFractions() {
+        Assertions.assertEquals(new Fraction(1, 6), new Fraction(1, 2).times(new Fraction(1, 3)));
+        Assertions.assertEquals(new Fraction(5, 4), new Fraction(1, 2).times(new Fraction(5, 2)));
     }
 }
