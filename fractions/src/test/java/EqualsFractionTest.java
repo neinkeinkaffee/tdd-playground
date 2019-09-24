6d4dcb106908ea9c3ad41aaf0315608d6a9283da
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-09-24 10:56:19 +0800

    Already equals 4 and 4

--- fractions/src/test/java/EqualsFractionTest.java
+++ fractions/src/test/java/EqualsFractionTest.java
@@ -6,4 +6,9 @@ public class EqualsFractionTest {
     void ZeroEqualsZero() {
         Assertions.assertEquals(new Fraction(0), new Fraction(0));
     }
+
+    @Test
+    void WholeNumberEqualsWholeNumber() {
+        Assertions.assertEquals(new Fraction(4), new Fraction(4));
+    }
 }
