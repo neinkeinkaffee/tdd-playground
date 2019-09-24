249bdd721ff0579d0e1bea2a6cdf8c5b0dabf337
Gesa Stupperich <gesa.stupperich@gmail.com>
2019-09-24 10:44:00 +0800

    Adds fractions with equal denominator

--- fractions/src/main/java/Fraction.java
+++ fractions/src/main/java/Fraction.java
@@ -1,15 +1,26 @@
 public class Fraction {
-    private int intValue;
+    private final int denominator;
+    private final int numerator;
 
     public Fraction(int numerator) {
-        this.intValue = numerator;
+        this.numerator = numerator;
+        this.denominator = 1;
     }
 
-    public Fraction plus(Fraction augend) {
-        return new Fraction(this.intValue + augend.intValue());
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
-    public int intValue() {
-        return intValue;
+    public Fraction plus(Fraction other) {
+        return new Fraction(this.numerator + other.getNumerator(), this.denominator);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
     }
 }
--- fractions/src/test/java/AddFractionsTest.java
+++ fractions/src/test/java/AddFractionsTest.java
@@ -5,24 +5,31 @@ public class AddFractionsTest {
     @Test
     void shouldAddZeroAndZero() {
         Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assertions.assertEquals(0, sum.intValue());
+        Assertions.assertEquals(0, sum.getNumerator());
     }
 
     @Test
     void shouldAddOneAndZero() {
         Fraction sum = new Fraction(1).plus(new Fraction(0));
-        Assertions.assertEquals(1, sum.intValue());
+        Assertions.assertEquals(1, sum.getNumerator());
     }
 
     @Test
     void shouldAddTwoPositiveWholeNumbers() {
         Fraction sum = new Fraction(1).plus(new Fraction(5));
-        Assertions.assertEquals(6, sum.intValue());
+        Assertions.assertEquals(6, sum.getNumerator());
     }
 
     @Test
     void shouldAddTwoWholeNumbers() {
         Fraction sum = new Fraction(1).plus(new Fraction(-5));
-        Assertions.assertEquals(-4, sum.intValue());
+        Assertions.assertEquals(-4, sum.getNumerator());
+    }
+
+    @Test
+    void shouldAddFractionsWithEqualDenominator() {
+        Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
+        Assertions.assertEquals(3, sum.getNumerator());
+        Assertions.assertEquals(5, sum.getDenominator());
     }
 }
